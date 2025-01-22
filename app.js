133a8f2d0d84d8b1a6926a4ee7a1cec283cd060d
davideryu2023 <davideryu2023@gmail.com>
2025-01-22 20:18:31 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 'use strict';
+const memo = new Map() //連想配列の作成
+memo.set(0, 0) // データの登録(key, value)
+memo.set(1, 1) // データの登録(key, value)
 function fib(n) {
-  if (n === 0) return 0;
-  if (n === 1) return 1;
-  return fib(n - 1) + fib(n - 2);
+  if (memo.has(n)) return memo.get(n)
+  // データも持っていない時
+  const value = fib(n-1) + fib(n-2);
+  memo.set(n, value) // データの登録(key, value)
+  return value; // 値を返す。
 }
 
-const length = 40;
+const length = 200;
 for (let i = 0; i <= length; i++) {
   console.log(fib(i));
 }
